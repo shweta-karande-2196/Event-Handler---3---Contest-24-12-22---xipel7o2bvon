56f15e56efc93a92435ca15f56e1bf0e54916f0e
shweta-karande-2196 <117108726+shweta-karande-2196@users.noreply.github.com>
2022-12-24 12:48:24 -0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,35 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  let [data, SetData]=useState({text:"",num:""})
-
-  const handleInput = (e) =>{
-   // use console.log
+let [data, SetData] = useState({
+  text:'',
+  num:''
+})
+  const handleInput = (event) =>{
     {
-       Setdata({
-      ...data,
+   // use console.log
+    SetData({
+    ...data,
       [e.target.name]:e.target.value
     })
-  };
   }
-
+    console.log(data);
+  }
   // do not change id of input elements
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input name="text" id="text-input" type={'text'} onChange={handleInput}/>
+      <input name="text" onChange={handleInput} id="text-input" type={'text'} />
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input name="num" id="num-input" onClick={handleInput} type={'number'} />
+      <input name="num" onChange={handleInput} id="num-input"  type={'number'} />
       <br/>
     </div>
   )
 }
+
 
 export default App;
